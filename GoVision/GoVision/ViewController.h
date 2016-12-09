dd2e6e5299ab777f0bcf440731206e813a7e1180
Nikulin Maxim <m.nikulin@rambler-co.ru>
2016-12-09 18:12:21 +0300

    Added project skeleton.

---
+++ GoVision/GoVision/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoVision
+//
+//  Created by Nikulin Maksim on 09/12/2016.
+//  Copyright © 2016 Rambler&Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
