e21a490dcc133ab29cddd477311bea54666beba3
Nikulin Maxim <m.nikulin@rambler-co.ru>
2016-12-11 00:30:09 +0300

    Added manual analize button on screen.

--- GoVision/GoVision/Classes/VIPER/Main/View/GVMainViewController.h
+++ GoVision/GoVision/Classes/VIPER/Main/View/GVMainViewController.h
@@ -19,8 +19,14 @@
 @property (nonatomic, weak) IBOutlet UIImageView *imageView;
 @property (nonatomic, weak) IBOutlet UILabel *resultLabel;
 
+@property (nonatomic, weak) IBOutlet UIButton *cameraButton;
+@property (nonatomic, weak) IBOutlet UIButton *galleryButton;
+@property (nonatomic, weak) IBOutlet UIButton *analizeButton;
+
 - (IBAction)cameraButtonWasPressed:(id)sender;
 
 - (IBAction)galleryButtonWasPressed:(id)sender;
+
+- (IBAction)analizeButtonWasPressed:(id)sender;
 
 @end
