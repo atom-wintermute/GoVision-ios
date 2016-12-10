12e204b4ab935714c0ac35cb4ac2dc76f415e457
Nikulin Maxim <m.nikulin@rambler-co.ru>
2016-12-10 23:59:45 +0300

    Added UIImage category for correct orientation.

---
+++ GoVision/GoVision/Classes/Extension/UIImage+OrientationFix.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+OrientationFix.h
+//  GoVision
+//
+//  Created by Nikulin Maksim on 10/12/2016.
+//  Copyright © 2016 Rambler&Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (OrientationFix)
+
+- (UIImage *)imageWithFixedOrientation;
+
+@end
